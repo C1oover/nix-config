ff22c6f5068d470069a3990cfdfcbdb143c5970a
C1oover <elias.reimoser@gmail.com>
2024-11-03 11:37:40 +0100

    add file system

--- hosts/azure/hardware-configuration.nix
+++ hosts/azure/hardware-configuration.nix
@@ -22,4 +22,24 @@
     "sd_mod"
   ];
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/e9ccdca7-3c4e-43a9-852f-4b3f3771d59e";
+      fsType = "ext4";
+    };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/A707-44B6";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
+
 }
